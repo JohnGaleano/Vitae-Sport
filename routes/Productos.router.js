e97912b8c8b32e8531adcb266cdea9a0eed4032f
John Galeano <jgaleanola@uninpahu.edu.co>
2021-11-18 18:11:07 -0500

    Creación de procesos de select,update e insert sobre controlador

--- routes/Productos.router.js
+++ routes/Productos.router.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const productosController = require("../Controllers/Productos.controller");
 
 router.post("/",productosController.create)
+router.get("/",productosController.find )
+router.get("/:id",productosController.findOne)
+router.put("/:id",productosController.update)
 
 module.exports = router
